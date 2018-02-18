d94023bb311f4d870879dc4e8a1d8c9d582c1e80
Ingvi Rafn Hafþórsson <ingvih@gmail.com>
2018-02-18 20:09:25 +0000

    :recycle: Moved code from App.js to pages/Home.js

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => (
+    <div className="App">
+        <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">Home page</p>
+    </div>
+)
+
+export default Home
